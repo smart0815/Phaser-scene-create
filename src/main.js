e1863498d9b2fbbc71ab9a40d34450eaadaff58c
smart0815 <sandy815.dev@outlook.com>
2021-10-14 17:25:05 +0800

    Create: main file

--- src/main.js
+++ src/main.js
@@ -1,18 +1,83 @@
 import Phaser from 'phaser'
 
 import HelloWorldScene from './scenes/HelloWorldScene'
+import GameScene from './scenes/GameScene'
 
-const config = {
-	type: Phaser.AUTO,
-	width: 800,
-	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 }
-		}
-	},
-	scene: [HelloWorldScene]
+class SceneA extends Phaser.Scene {
+
+    constructor ()
+    {
+        super('GameScene');
+    }
+
+    preload ()
+    {
+        this.load.image('bg', './assets/skies/sky4.png');
+        this.load.image('crate', './assets/sprites/crate.png');
+    }
+
+    create ()
+    {
+        this.add.image(400, 300, 'bg');
+
+        for (let i = 0; i < 64; i++)
+        {
+            let x = Phaser.Math.Between(0, 800);
+            let y = Phaser.Math.Between(0, 600);
+
+            this.add.image(x, y, 'crate').setInteractive();
+        }
+
+        this.input.on('gameobjectup', this.clickHandler, this);
+    }
+
+    clickHandler (pointer, box)
+    {
+        //  Disable our box
+        box.input.enabled = false;
+        box.setVisible(false);
+
+        //  Dispatch a Scene event
+        this.events.emit('addScore');
+    }
+
 }
 
-export default new Phaser.Game(config)
+class SceneB extends Phaser.Scene {
+
+    constructor ()
+    {
+        super({ key: 'UIScene', active: true });
+
+        this.score = 0;
+    }
+
+    create ()
+    {
+        //  Our Text object to display the Score
+        let info = this.add.text(10, 10, 'Score: 0', { font: '48px Arial' });
+
+        //  Grab a reference to the Game Scene
+        let ourGame = this.scene.get('GameScene');
+
+        //  Listen for events from it
+        ourGame.events.on('addScore', function () {
+
+            this.score += 10;
+
+            info.setText('Score: ' + this.score);
+
+        }, this);
+    }
+}
+
+let config = {
+    type: Phaser.AUTO,
+    width: 1800,
+    height: 600,
+    backgroundColor: '#ff00ff',
+    parent: 'phaser-example',
+    scene: [ SceneA, SceneB ]
+};
+
+let game = new Phaser.Game(config);
